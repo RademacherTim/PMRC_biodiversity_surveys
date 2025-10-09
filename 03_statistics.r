96f2109e5a648426506a0e1bbec7630b6dd8ead5
RademacherTim <tim.rademacher@uvm.edu>
2025-10-09 12:07:03 -0400

    Updated my script for the ploting of the dead and alive trees.

--- 03_statistics.r
+++ 03_statistics.r
@@ -7,19 +7,35 @@ source('01_read_data.R')
 # load dependency ----
 library('lme4')
 
-# making a plot ----
-par(mar = c(5, 5, 1, 1))
-plot (d_tree$dbh, d_tree$n_TreMs, xlim = c(0, 100), axes = FALSE, 
+# plot of the number of TreMs versus DBH ----
+par(mar = c(5, 5, 1, 1), mfrow = c(1, 2))
+plot (x = d_tree$dbh[d_tree$alive == "T"], 
+      y = d_tree$n_TreMs[d_tree$alive == "T"], 
+      xlim = c(0, 100), axes = FALSE, ylim = c(0, 15),
       xlab = "Diameter at breast height (cm)",
       ylab = "Number of TreMs", pch = 19, col = "#91b9a499")
+text(x = 100, y = 15, pos = 2, labels = "Alive")
 axis(side = 1)
 axis(side = 2, las = 1)
 
-fit <- lm(n_TreMs ~ dbh, data = d_tree)
+fit_alive <- lm(n_TreMs ~ dbh, data = d_tree[d_tree$alive == "T", ])
+abline(fit_alive, col = "#91b9a4", lty = 2, lwd = 2)
 
-abline(fit, col = "red")
+par(mar = c (5, 1, 1, 1))
+plot (x = d_tree$dbh[d_tree$alive == "F"], 
+      y = d_tree$n_TreMs[d_tree$alive == "F"], 
+      xlim = c(0, 100), axes = FALSE, ylim = c(0, 15), 
+      xlab = "Diameter at breast height (cm)",
+      ylab = "", pch = 19, col = "#C46F33")
+axis(side = 1)
+axis(side = 2, las = 1)
+text(x = 100, y = 15, pos = 2, labels = "Dead")
 
-fit2 <- lmer (n_TreMs ~ dbh + (1 | spp), data = d_tree)
+fit_dead <- lm(n_TreMs ~ dbh, data = d_tree[d_tree$alive == "F", ])
+abline(fit_dead, col = "#C46F33", lty = 2, lwd = 2)
+
+
+fit2 <- lmer(n_TreMs ~ dbh + (1 | spp), data = d_tree)
 coef(fit2)
 abline(fit2)
 
