c81280b049b40cf6455be0800d68397a93b20946
RademacherTim <tim.rademacher@uvm.edu>
2025-10-16 10:24:37 -0400

    Changes to the graphs to break them up by species.

--- 03_statistics.r
+++ 03_statistics.r
@@ -7,33 +7,77 @@ source('01_read_data.R')
 # load dependency ----
 library('lme4')
 
-# plot of the number of TreMs versus DBH ----
-par(mar = c(5, 5, 1, 1), mfrow = c(1, 2))
-plot (x = d_tree$dbh[d_tree$alive == "T"], 
-      y = d_tree$n_TreMs[d_tree$alive == "T"], 
-      xlim = c(0, 100), axes = FALSE, ylim = c(0, 15),
+# remove the data for trees that we do not have at least data for 20 
+# individuals from that species
+d_tmp <- d_tree %>% filter(spp %in% c("American Beech", 
+                                      "Red Maple", 
+                                      "Red Spruce",
+                                      "Sugar Maple",
+                                      "White Ash",
+                                      "Yellow Birch"))
+
+# plot of the number of TreMs versus DBH for six species with more than 20 
+# individuals ----
+par(mfrow = c(6, 2))
+for (species in c("American Beech", "Red Maple", "Red Spruce", "Sugar Maple", "White Ash", "Yellow Birch")){
+  par(mar = c(5, 5, 1, 1))
+  con <- d_tmp$alive & d_tmp$spp == species
+  plot (x = d_tmp$dbh[con], 
+        y = d_tmp$n_TreMs[con], 
+        xlim = c(0, 100), axes = FALSE, ylim = c(0, 16),
+        xlab = "Diameter at breast height (cm)",
+        ylab = "Number of TreMs", pch = 19, col = "#91b9a499")
+  text(x = 20, y = 16, pos = 2, labels = "Alive")
+  axis(side = 1)
+  axis(side = 2, las = 1)
+  
+  fit_alive <- lm(n_TreMs ~ dbh, data = d_tmp[d_tmp$alive, ])
+  abline(fit_alive, col = "#91b9a4", lty = 2, lwd = 2)
+  
+  par(mar = c (5, 1, 1, 1))
+  con <- !d_tmp$alive & d_tmp$spp == species
+  plot (x = d_tmp$dbh[con], 
+        y = d_tmp$n_TreMs[con], 
+        xlim = c(0, 100), axes = FALSE, ylim = c(0, 16), 
+        xlab = "Diameter at breast height (cm)",
+        ylab = "", pch = 19, col = "#C46F33")
+  axis(side = 1)
+  axis(side = 2, las = 1)
+  text(x = 20, y = 16, pos = 2, labels = "Dead")
+  
+  fit_dead <- lm(n_TreMs ~ dbh, data = d_tmp[!d_tmp$alive, ])
+  abline(fit_dead, col = "#C46F33", lty = 2, lwd = 2)
+
+}
+
+# Red Maple ----
+par (mar = c(5, 5, 1, 1))
+con <- d_tmp$alive & d_tmp$spp == "Red Maple"
+plot (x = d_tmp$dbh[con], 
+      y = d_tmp$n_TreMs[con], 
+      xlim = c(0, 100), axes = FALSE, ylim = c(0, 16),
       xlab = "Diameter at breast height (cm)",
       ylab = "Number of TreMs", pch = 19, col = "#91b9a499")
-text(x = 100, y = 15, pos = 2, labels = "Alive")
+text(x = 20, y = 16, pos = 2, labels = "Alive")
 axis(side = 1)
 axis(side = 2, las = 1)
 
-fit_alive <- lm(n_TreMs ~ dbh, data = d_tree[d_tree$alive == "T", ])
+fit_alive <- lm(n_TreMs ~ dbh, data = d_tmp[d_tmp$alive, ])
 abline(fit_alive, col = "#91b9a4", lty = 2, lwd = 2)
 
 par(mar = c (5, 1, 1, 1))
-plot (x = d_tree$dbh[d_tree$alive == "F"], 
-      y = d_tree$n_TreMs[d_tree$alive == "F"], 
-      xlim = c(0, 100), axes = FALSE, ylim = c(0, 15), 
+con <- !d_tmp$alive & d_tmp$spp == "Red Maple"
+plot (x = d_tmp$dbh[con], 
+      y = d_tmp$n_TreMs[con], 
+      xlim = c(0, 100), axes = FALSE, ylim = c(0, 16), 
       xlab = "Diameter at breast height (cm)",
       ylab = "", pch = 19, col = "#C46F33")
 axis(side = 1)
 axis(side = 2, las = 1)
-text(x = 100, y = 15, pos = 2, labels = "Dead")
+text(x = 20, y = 16, pos = 2, labels = "Dead")
 
-fit_dead <- lm(n_TreMs ~ dbh, data = d_tree[d_tree$alive == "F", ])
+fit_dead <- lm(n_TreMs ~ dbh, data = d_tmp[!d_tmp$alive, ])
 abline(fit_dead, col = "#C46F33", lty = 2, lwd = 2)
-
 
 fit2 <- lmer(n_TreMs ~ dbh + (1 | spp), data = d_tree)
 coef(fit2)
